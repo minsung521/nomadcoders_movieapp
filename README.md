26120f67a45c418858d03e282f17a9011e9ab89f
kms0201 <kmskmskmskgb@gmail.com>
2020-10-20 15:11:49 +0900

    #3.2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## REACT movieapp tutorials
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count : 0
+  };
+  add = () => {
+    console.log("add");
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState(current => ({count: current.count - 1}));
+  };
+  render(){
+    return (
+      <div>  
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
