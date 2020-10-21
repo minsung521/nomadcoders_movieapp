5eaeb63dbe7e6b810d0fa7a6475799386f56a3ee
kms0201 <kmskmskmskgb@gmail.com>
2020-10-21 14:11:55 +0900

    #3.3

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import React from 'react';
 //import PropTypes from "prop-types";
 
 class App extends React.Component {
-  state = {
-    count : 0
-  };
-  add = () => {
-    console.log("add");
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    console.log("minus");
-    this.setState(current => ({count: current.count - 1}));
-  };
+  state={
+    isLoading : true,
+    movies : []
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    },6000);
+  }
   render(){
-    return (
-      <div>  
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
+    const {isLoading} = this.state;
+    return(
+    <div>{isLoading ? "Loading":"We are Ready"}</div>
     );
   }
 }
+
 
 export default App;
